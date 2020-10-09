a23820c4bcc07197bc966371956c0cd2ff273ae8
trise <1957769588@qq.com>
2020-10-09 12:53:17 +0800

    Initial commit

---
+++ server.py
@@ -0,0 +1,30 @@
+'''
+@author: trise
+@studioe: JCAI
+@software: pycharm
+@time: 2020/10/7 15:15
+'''
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import features
+import os
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
+
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(
+        handlers = [
+            (r"/Index", features.IndexHandler),
+            (r"/PicturePlatform", features.PicHandler),
+            (r"/VideoPlatform", features.VideoHandler),
+        ],
+        static_path = os.path.join(os.path.dirname(__file__), "static"),
+        template_path = os.path.join(os.path.dirname(__file__), "templates")
+    )
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
